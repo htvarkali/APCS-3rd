013d7a1eb9f17a3fb75b62c849a29e08c13b8313
htvarkali <64799163+htvarkali@users.noreply.github.com>
2025-08-12 11:15:21 -0700

    Add files via upload

---
+++ Initials/src/Main.java
@@ -0,0 +1,108 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+import kareltherobot.Directions;
+import kareltherobot.Robot;
+import kareltherobot.World;
+
+public class Main implements Directions {
+   public Main() {
+   }
+
+   public static void main(String[] args) {
+      World.setVisible(true);
+      World.setDelay(0);
+      Robot r = new Robot(9,1,East,1000);
+   
+    // examples of commands you can invoke on a Robot
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.move();
+    r.turnLeft();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.move();
+    r.move();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    
+
+
+   
+   
+
+
+
+
+
+
+   
+   
+   
+
+
+  }
+}
